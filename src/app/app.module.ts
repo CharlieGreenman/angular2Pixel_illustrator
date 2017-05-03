70a0aea47dabe61f98bdafec9a30a71da9bd198e
Charlie Greenman <charie@razroo.com>
2017-05-02 22:22:23 -0400

    Update the color-picker module to have appropriate file paths and bootstrapping.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ColorPickerModule } from './components/color-picker/color-picker.module';
 
 import { AppComponent } from './app.component';
 
@@ -19,6 +20,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ColorPickerModule,
     StoreModule.provideStore({environment}),
     StoreDevtoolsModule.instrumentOnlyWithExtension({
       maxAge: 5
--- src/app/components/color-picker/color-picker.module.ts
+++ src/app/components/color-picker/color-picker.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ColorPickerComponent } from './color-picker.component';
+
 import { BackgroundColorPickerComponent } from './background-color-picker/background-color-picker.component';
 import { PixelColorPickerComponent } from './pixel-color-picker/pixel-color-picker.component';
 import { ColorPickerColumnComponent } from './color-picker-column/color-picker-column.component';
@@ -15,6 +17,6 @@ import { ColorPickerColumnComponent } from './color-picker-column/color-picker-c
     ColorPickerColumnComponent
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [ColorPickerComponent]
 })
 export class ColorPickerModule { }
