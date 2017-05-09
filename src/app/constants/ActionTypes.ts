0fbe4a9922bc9ed21e03d7a982750e598c47ae08
Charlie Greenman <charie@razroo.com>
2017-05-08 21:35:04 -0400

    Update reducer for color picker to have proper constants imported.

--- src/app/constants/ActionTypes.ts
+++ src/app/constants/ActionTypes.ts
@@ -1,2 +1,8 @@
 export const HIDE = 'hide';
 export const SHOW = 'show';
+
+export const BACKGROUND_COLOR = "BACKGROUND_COLOR";
+export const BACKGROUND_RGB_COLOR = "BACKGROUND_RGB_COLOR";
+export const PIXEL_COLOR = "PIXEL_COLOR";
+export const CONVERTED_PIXEL_RGB_COLOR = "CONVERTED_PIXEL_RGB_COLOR";
+export const PIXEL_RGB_COLOR = "PIXEL_RGB_COLOR";
