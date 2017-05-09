ccf6688d34a60dc2797f18b36039c2227eaef030
Charlie Greenman <charie@razroo.com>
2017-05-08 21:28:31 -0400

    Add color-picker reducer to app.

---
+++ src/app/reducers/color-picker.ts
@@ -0,0 +1,37 @@
+const initialColorState = {
+    backgroundHex: "#191919",
+    backgroundRed: "25",
+    backgroundGreen: "25",
+    backgroundBlue: "25",
+    pixelHex: "#000000",
+    red: "25",
+    green: "25",
+    blue: "25"
+};
+
+export const colors = (state = initialColorState, action) => {
+  switch(action.type) {
+    case types.BACKGROUND_COLOR:
+      return Object.assign({}, state, {
+          backgroundHex: action.backgroundHex || state.backgroundHex
+      });
+    case types.PIXEL_COLOR:
+      return Object.assign({}, state, {
+          pixelHex: action.pixelHex || state.pixelHex
+      });
+    case types.BACKGROUND_RGB_COLOR:
+      return Object.assign({}, state, {
+          backgroundRed: action.backgroundRed || state.backgroundRed,
+          backgroundGreen: action.backgroundGreen || state.backgroundGreen,
+          backgroundBlue: action.backgroundBlue || state.backgroundBlue,
+      });
+    case types.PIXEL_RGB_COLOR:
+      return Object.assign({}, state, {
+          pixelRed: action.red || state.red,
+          pixelGreen: action.green || state.green,
+          pixelBlue: action.blue || state.blue,
+      });
+    default:
+      return state;
+  }
+};
