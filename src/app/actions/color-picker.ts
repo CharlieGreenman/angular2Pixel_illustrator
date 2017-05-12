1e3dc8fc3ef87e50f5bf75ad528d5fabffb9bc6a
Charlie Greenman <charie@razroo.com>
2017-05-11 21:04:20 -0400

    Add color-picker actions.

---
+++ src/app/actions/color-picker.ts
@@ -0,0 +1,33 @@
+import * as types from "../constants/ActionTypes";
+
+export function BackgroundColor(color){
+    return{
+        type: types.BACKGROUND_COLOR,
+        backgroundHex: color
+    }
+}
+
+export function BackgroundColorRGB(r,g,b){
+    return{
+        type: types.BACKGROUND_RGB_COLOR,
+        backgroundRed: r,
+        backgroundGreen: g,
+        backgroundBlue: b
+    }
+}
+
+export function PixelColor(color){
+    return{
+        type: types.PIXEL_COLOR,
+        pixelHex: color
+    }
+}
+
+export function PixelColorRGB(r,g,b){
+    return{
+        type: types.PIXEL_RGB_COLOR,
+        red: r,
+        green: g,
+        blue: b
+    }
+}
