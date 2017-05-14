89438d5e80cfae19102d965bd7ce71aa759e1a83
Charlie Greenman <charie@razroo.com>
2017-05-13 23:43:30 -0400

    Modify color-converter service for code illustrator app.

--- src/app/core/color-converter-svc.service.ts
+++ src/app/core/color-converter-svc.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ColorConverterSvcService {
+  hexToRgb;
 
-  constructor() { }
+  constructor() {
+
+    this.hexToRgb = (hex) => {
+      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+        return r + r + g + g + b + b;
+      });
+    };
+
+  }
 
 }
