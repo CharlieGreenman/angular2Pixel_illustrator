329204de7077b48cbdae8c058d2a438dee3601a1
Charlie Greenman <charie@razroo.com>
2017-05-15 21:53:33 -0400

    Update the functionality for the hexToRgb functionality to be recemt. Also to not mutate state.

--- src/app/components/color-picker/background-color-picker/background-color-picker.component.ts
+++ src/app/components/color-picker/background-color-picker/background-color-picker.component.ts
@@ -8,7 +8,7 @@ import { Subject } from 'rxjs/Subject';
 import { Store } from '@ngrx/store';
 
 import { ColorConverterSvcService } from '../../../core/color-converter-svc.service';
-import { BackgroundColor } from '../../../actions/color-picker';
+import { BackgroundColor, BackgroundColorRGB } from '../../../actions/color-picker';
 
 @Component({
   selector: 'app-background-color-picker',
@@ -20,12 +20,16 @@ export class BackgroundColorPickerComponent implements OnInit {
   colors;
   changeBackgroundColor$ = new Subject();
 
-  constructor(store: Store<any>, private colorConverterSvcService: ColorConverterSvcService) {
+  constructor(store: Store<any>, colorConverterSvcService: ColorConverterSvcService) {
     this.colors = store.select('colors');
     console.log('background color initialized: ' + JSON.stringify(colorConverterSvcService.hexToRgb('#123123')));
 
     Observable.merge(
-      this.changeBackgroundColor$.map((value) => (BackgroundColor(value)))
+      this.changeBackgroundColor$.map((value) => (BackgroundColor(value),
+        BackgroundColorRGB(colorConverterSvcService.hexToRgb(value).r,
+        colorConverterSvcService.hexToRgb(value).g,
+        colorConverterSvcService.hexToRgb(value).b)
+      ))
     )
     .subscribe((action)=>{
       console.log('action: ' + JSON.stringify(action, null, 2))
--- src/app/core/color-converter-svc.service.ts
+++ src/app/core/color-converter-svc.service.ts
@@ -4,20 +4,17 @@ import { Injectable } from '@angular/core';
 export class ColorConverterSvcService {
 
   hexToRgb(hex): any {
-    console.log('hexToRgb called');
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    let shorthandRegex: any =  /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-      return r + r + g + g + b + b;
-    });
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    var arrBuff = new ArrayBuffer(4);
+    var vw = new DataView(arrBuff);
+    vw.setUint32(0,parseInt(hex, 16),false);
+    var arrByte = new Uint8Array(arrBuff);
 
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
+    return {
+      r: arrByte[1],
+      g: arrByte[2],
+      b: arrByte[3]
+    };
   }
 
 }
