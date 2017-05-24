a37a3e6dc81e031b424947144d3ac5dd0af14ce8
Charlie Greenman <charlie@razroo.com>
2017-05-24 19:00:44 -0400

    Add the utils file as needed functionality for the color-picker functionality.

---
+++ src/app/core/utils/color-converter.ts
@@ -0,0 +1,13 @@
+export function hexToRgb(hex): any {
+
+  var arrBuff = new ArrayBuffer(4);
+  var vw = new DataView(arrBuff);
+  vw.setUint32(0,parseInt(hex, 16),false);
+  var arrByte = new Uint8Array(arrBuff);
+
+  return {
+    r: arrByte[1],
+    g: arrByte[2],
+    b: arrByte[3]
+  };
+}
