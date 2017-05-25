5bfa8a291c47d74075f16827bb5d072f9ed35d2e
Charlie Greenman <charlie@razroo.com>
2017-05-25 12:48:47 -0400

    Update the color picker functionality to be able to have the pount sign as included as part of it's functionality.

--- src/app/core/utils/color-converter.ts
+++ src/app/core/utils/color-converter.ts
@@ -1,8 +1,8 @@
 export function hexToRgb(hex): any {
-
+  var currentHex  = hex.split('#')[1];
   var arrBuff = new ArrayBuffer(4);
   var vw = new DataView(arrBuff);
-  vw.setUint32(0,parseInt(hex, 16),false);
+  vw.setUint32(0,parseInt(currentHex, 16),false);
   var arrByte = new Uint8Array(arrBuff);
 
   return {
