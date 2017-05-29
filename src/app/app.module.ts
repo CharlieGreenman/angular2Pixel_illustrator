253ce4247aacc0c6fbe884841cae233d23a08987
Charlie Greenman <charie@razroo.com>
2017-05-29 18:38:45 -0400

    Properly hook up pixel-grid component into app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ColorPickerModule } from './components/color-picker/color-picker.module';
+import { PixelGridModule } from './components/pixel-grid/pixel-grid.module';
 
 import { AppComponent } from './app.component';
 import { ShowHideBarComponent } from './show-hide-bar/show-hide-bar.component';
@@ -13,19 +14,18 @@ import { environment } from './reducers/show-hide';
 import { colors } from './reducers/color-picker';
 
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { PixelGridComponent } from './components/pixel-grid/pixel-grid/pixel-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowHideBarComponent,
-    PixelGridComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     ColorPickerModule,
+    PixelGridModule,
     StoreModule.provideStore({environment, colors}),
     StoreDevtoolsModule.instrumentOnlyWithExtension({
       maxAge: 5
--- src/app/components/pixel-grid/pixel-grid.module.ts
+++ src/app/components/pixel-grid/pixel-grid.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PixelGridComponent } from './pixel-grid.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    PixelGridComponent
+  ],
+  exports: [
+    PixelGridComponent
+  ],
+  providers: []
+
 })
 export class PixelGridModule { }
