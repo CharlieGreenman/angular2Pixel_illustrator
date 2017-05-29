71233c8c42d9ed792b8aab3949fdbac802a0b713
Charlie Greenman <charie@razroo.com>
2017-05-28 21:46:24 -0400

    Benchmark progress for setting up obseravables for the pixel color component.

--- src/app/components/color-picker/pixel-color-picker/pixel-color-picker.component.ts
+++ src/app/components/color-picker/pixel-color-picker/pixel-color-picker.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/map';
+import { Subject } from 'rxjs/Subject';
+import { Store } from '@ngrx/store';
+
+import { ColorConverterSvcService } from '../../../core/color-converter-svc.service';
+
 @Component({
   selector: 'app-pixel-color-picker',
   templateUrl: './pixel-color-picker.component.html',
   styleUrls: ['./pixel-color-picker.component.scss']
 })
 export class PixelColorPickerComponent implements OnInit {
+  colors;
+  changePixelColor$ = new Subject();
+  changePixelColorRGB$ = new Subject();
 
   constructor() { }
 
