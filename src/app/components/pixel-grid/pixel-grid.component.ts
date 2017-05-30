c28424eddc0015c74344a3061777116502665207
Charlie Greenman <charlie@razroo.com>
2017-05-30 13:17:52 -0400

    Benchmark progress for being able to supply columns, row, and pixel size to the pixel-grid component.

--- src/app/components/pixel-grid/pixel-grid.component.ts
+++ src/app/components/pixel-grid/pixel-grid.component.ts
@@ -8,11 +8,9 @@ import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 export class PixelGridComponent implements OnInit {
   @ViewChild('canvas') canvasRef:ElementRef;
   private canvas: any;
-  @Input() data: number[];
-  @Input() width: number;
-  @Input() height: number;
-  @Input() colors: string[];
-  @Input() title: string;
+  @Input() row: number;
+  @Input() column: number;
+  @Input() pixelSize: number;
 
   constructor() { }
 
