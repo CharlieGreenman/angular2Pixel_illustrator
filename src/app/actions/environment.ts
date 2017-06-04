3fb4334dc4c520f2d4f5e974f0efe9b905df96a5
Charlie Greenman <charlie@razroo.com>
2017-06-04 14:36:34 -0400

    Add appropriate actions and constants for the choose-size component.

---
+++ src/app/actions/environment.ts
@@ -0,0 +1,35 @@
+import * as types from "../constants/ActionTypes";
+
+//singular state for all three applications
+//change element so that element can be where it needs to be
+export function InitGrid(init) {
+  return {
+      type: types.INIT_APP,
+      init
+  };
+}
+
+//three input fields for form
+//columncount
+export function ColumnCount(column) {
+  return {
+      type: types.COLUMN_COUNT,
+      count: column
+  };
+}
+
+//rowcount
+export function RowCount(row) {
+  return {
+      type: types.ROW_COUNT,
+      count: row
+  };
+}
+
+//rowcount
+export function PixelSize(pixel) {
+  return {
+      type: types.PIXEL_SIZE,
+      count: pixel
+  };
+}
--- src/app/constants/ActionTypes.ts
+++ src/app/constants/ActionTypes.ts
@@ -1,6 +1,12 @@
 export const HIDE = 'hide';
 export const SHOW = 'show';
 
+export const INIT_APP = "INIT_APP";
+
+export const COLUMN_COUNT = "COLUMN_COUNT";
+export const ROW_COUNT = "ROW_COUNT";
+export const PIXEL_SIZE = "PIXEL_SIZE";
+
 export const BACKGROUND_COLOR = "BACKGROUND_COLOR";
 export const BACKGROUND_RGB_COLOR = "BACKGROUND_RGB_COLOR";
 export const PIXEL_COLOR = "PIXEL_COLOR";
