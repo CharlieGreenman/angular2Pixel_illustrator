8f4c97208c089c23706645ca91d954947d87c538
Charlie Greenman <charlie@razroo.com>
2017-06-06 13:24:46 -0400

    Benchmark progress for hooking canvas div into angular app.

--- src/app/components/pixel-grid/pixel-grid.component.ts
+++ src/app/components/pixel-grid/pixel-grid.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './pixel-grid.component.html',
   styleUrls: ['./pixel-grid.component.scss']
 })
-export class PixelGridComponent implements OnInit {
+export class PixelGridComponent {
   @ViewChild('canvas') canvasRef:ElementRef;
   private canvas: any;
   @Input() row: number;
@@ -15,7 +15,19 @@ export class PixelGridComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log('current row size: ' + this.row);
+
   }
+
+  ngAfterViewInit() {
+    console.log('current row size: ' + this.canvas);
+    const c = this.canvas.getContext('2d');
+    c.fillStyle = "#fefefe";
+    console.log('ngAfterViewInit called');
+  }
+
+  // if(this.canvas.getContext) {
+  //   console.log('canvas has context and such');
+  // }
+
 
 }
