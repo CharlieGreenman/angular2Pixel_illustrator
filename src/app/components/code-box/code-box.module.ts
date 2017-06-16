d3b2b956a31f27bfa526d35fdea00f4741ed82d9
Charlie Greenman <charie@razroo.com>
2017-06-15 22:55:14 -0400

    Benchmark progress for creating code-box component.

--- src/app/components/code-box/code-box.module.ts
+++ src/app/components/code-box/code-box.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CodeBoxComponent } from './code-box.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    CodeBoxComponent
+  ],
+  exports: [
+    CodeBoxComponent
+  ],
+  providers: []
 })
 export class CodeBoxModule { }
--- src/app/components/grid-illustrator/grid-illustrator.module.ts
+++ src/app/components/grid-illustrator/grid-illustrator.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PixelGridModule } from '../pixel-grid/pixel-grid.module';
 import { ColorPickerModule } from '../color-picker/color-picker.module';
 import { ChooseSizeModule } from '../choose-size/choose-size.module';
+import { CodeBoxModule } from '../code-box/code-box.module';
 
 import { GridIllustratorComponent } from './grid-illustrator.component';
 
@@ -12,7 +13,8 @@ import { GridIllustratorComponent } from './grid-illustrator.component';
     CommonModule,
     PixelGridModule,
     ColorPickerModule,
-    ChooseSizeModule
+    ChooseSizeModule,
+    CodeBoxModule
   ],
   declarations: [
     GridIllustratorComponent
