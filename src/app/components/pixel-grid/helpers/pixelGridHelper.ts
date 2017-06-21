3e587ff7a6498bdc9180bb171f08a1d4bc6909cc
Charlie Greenman <charlie@razroo.com>
2017-06-21 13:06:56 -0400

    Create a function to create coordinates for the pixel grid.

--- src/app/components/pixel-grid/helpers/pixelGridHelper.ts
+++ src/app/components/pixel-grid/helpers/pixelGridHelper.ts
@@ -1,3 +1,7 @@
-export function hexToRgb(hex): any {
-
+function determineCoordinate(event, direction): number {
+  event = event || window.event;
+  return Math.floor(event.offsetX === undefined ? event.layer[direction] : event.offset[direction] / this.pixelSize) * this.pixelSize;
 }
+
+let xVal: number = determineCoordinate(event, 'x');
+let yVal: number = determineCoordinate(event, 'y');
