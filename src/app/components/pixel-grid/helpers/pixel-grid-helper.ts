b51e81f7a189c54b1329ddee1c2c5dbcb1114e9e
Charlie Greenman <charlie@razroo.com>
2017-06-22 13:21:43 -0400

    Add the determinePixelOffset function.

--- src/app/components/pixel-grid/helpers/pixel-grid-helper.ts
+++ src/app/components/pixel-grid/helpers/pixel-grid-helper.ts
@@ -2,3 +2,14 @@ export function determineCoordinate(event, direction): number {
   event = event || window.event;
   return Math.floor(event.offsetX === undefined ? event.layer[direction] : event.offset[direction] / this.pixelSize) * this.pixelSize;
 }
+
+function determinePixelOffset(event, pixelSize, direction): number {
+  return Math.floor(event.offset[direction] / pixelSize) * pixelSize + 1;
+}
+
+export function determinePixelRGB(): string {
+  return ctx.getImageData(Math.floor(e.offsetX / elem.s.pixSize) * elem.s.pixSize + 1,
+            Math.floor(e.offsetY / elem.s.pixSize) * elem.s.pixSize + 1,
+            elem.s.pixSize - 2, elem.s.pixSize - 2);
+
+}
