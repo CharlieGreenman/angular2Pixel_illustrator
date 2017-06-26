61280788adc9a1e1b2282d77572794f3cd07d232
Charlie Greenman <charlie@razroo.com>
2017-06-26 12:51:30 -0400

    Benchmark progress for styling to properly center canvas, and more fluid style architecture.

--- src/app/components/pixel-grid/helpers/pixel-grid-helper.ts
+++ src/app/components/pixel-grid/helpers/pixel-grid-helper.ts
@@ -7,9 +7,9 @@ function determinePixelOffset(event, pixelSize, direction): number {
   return Math.floor(event.offset[direction] / pixelSize) * pixelSize + 1;
 }
 
-export function determinePixelRGB(): string {
-  return ctx.getImageData(Math.floor(e.offsetX / elem.s.pixSize) * elem.s.pixSize + 1,
-            Math.floor(e.offsetY / elem.s.pixSize) * elem.s.pixSize + 1,
+export function determinePixelRGB(ctx, event, pixelSize): string {
+  return ctx.getImageData(determinePixelOffset(event, pixelSize, 'X'),
+            determinePixelOffset(event, pixelSize, 'Y'),
             elem.s.pixSize - 2, elem.s.pixSize - 2);
 
 }
