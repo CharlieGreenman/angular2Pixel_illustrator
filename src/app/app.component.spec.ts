e60d9eb8c7a8e3c8783c7745a7c5e4442885cf8d
Charlie Greenman <charie@razroo.com>
2017-07-10 21:55:20 -0400

    Benchmark progress for completing unit testing for the angular2 app.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,12 +3,30 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { GridIllustratorModule } from './components/grid-illustrator/grid-illustrator.module';
+
+import { StoreModule } from '@ngrx/store';
+import { environment } from './reducers/show-hide';
+import { colors } from './reducers/color-picker';
+import { gridSettings } from './reducers/grid-settings';
+
+import { Routes, RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        AppRoutingModule,
+        GridIllustratorModule,
+        RouterModule,
+        StoreModule.provideStore({environment, colors, gridSettings})
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
     });
     TestBed.compileComponents();
   });
