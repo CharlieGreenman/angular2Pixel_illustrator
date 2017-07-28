adeb1a0d015542804c4bad154b154a7c16c53a89
Charlie Greenman <charie@razroo.com>
2017-07-27 21:48:50 -0400

    Set up spec test for the color converter service.

--- src/app/core/color-converter-svc.service.spec.ts
+++ src/app/core/color-converter-svc.service.spec.ts
@@ -1,15 +1,13 @@
+
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ColorConverterSvcService } from './color-converter-svc.service';
 
 describe('ColorConverterSvcService', () => {
-  let colorConverterSvcService: ColorConverterSvcService;
-
   beforeEach(() => {
-    mockColorConverterSvcService = new ColorConverterSvcService();
-    TestBed.configureTestingModule(
-      {provide: ColorConverterSvcService, useValue: mockColorConverterSvcService}
+    TestBed.configureTestingModule({
+      providers: [ColorConverterSvcService]
     });
   });
 
@@ -18,8 +16,9 @@ describe('ColorConverterSvcService', () => {
   }));
 
   describe('functionality for the hexToRGB Function', function() {
-    fit('should convert hex to rgb.', inject([ColorConverterSvcService], (service: ColorConverterSvcService) => {
-      expect(mockColorConverterSvcService.hexToRGB('#123123')).toEqual('123');
-    }));
+    fit('should convert hex to rgb.', inject([ColorConverterSvcService],
+      (service: ColorConverterSvcService) => {
+
+      expect(service.hexToRgb('#123123')).toEqual('123'));
+    });
   });
-});
