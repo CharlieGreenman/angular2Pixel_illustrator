112a1200e42980c9dba2d2cf1c7320ac3b53a521
Charlie Greenman <charlie@razroo.com>
2017-12-14 13:03:54 -0500

    Benchmark progress towards creating a proper grid-settings class.

--- src/app/actions/grid-settings.ts
+++ src/app/actions/grid-settings.ts
@@ -3,10 +3,16 @@ import { Action } from '@ngrx/store';
 
 // export class gridSettings implements Action {
 //   readonly type = types.GRID_SETTINGS;
+//
+//   constructor(public columnCount: number, public rowCount: number, public pixelSize: number) {
+//     columnCount: columnCount;
+//     rowCount: rowCount;
+//     pixelSize: pixelSize;
+//   }
 // }
 
-export type All
-  = gridSettings;
+// export type All
+//   = gridSettings;
 
 export function gridSettings(columnCount: number, rowCount: number, pixelSize: number) {
   return {
