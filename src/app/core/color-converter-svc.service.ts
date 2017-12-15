7b729fac1f01368b6a52a522236a163d53458b16
Charlie Greenman <charlie@razroo.com>
2017-12-15 09:40:29 -0500

    Add a componentToHex function.

--- src/app/core/color-converter-svc.service.ts
+++ src/app/core/color-converter-svc.service.ts
@@ -19,4 +19,15 @@ export class ColorConverterSvcService {
     } : null;
   }
 
+  componentToHex(c) : any{
+    console.log("c: " + c);
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+
+  rgbToHex(r, g, b) {
+    return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+  }
+
+
 }
