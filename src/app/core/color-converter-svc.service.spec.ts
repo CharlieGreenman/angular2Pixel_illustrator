1549d38e03b9879cad714def99080abfb8a9681a
Charlie Greenman <charlie@razroo.com>
2017-12-15 10:24:26 -0500

    Update the color converter service spec service syntax as a result of removing service from being injected in every test block.

--- src/app/core/color-converter-svc.service.spec.ts
+++ src/app/core/color-converter-svc.service.spec.ts
@@ -16,24 +16,24 @@ describe('ColorConverterSvcService', () => {
 
   it('should ...', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 
   describe('functionality for the hexToRGB Function', function() {
     it('should convert white hex to white equivalent in rgb.',() => {
       expect(service.hexToRgb('#ffffff')).toEqual({ r: 255, g: 255, b: 255 });
-    }));
+    });
 
     it('should convert black hex to black equivalent in rgb.', () => {
       expect(service.hexToRgb('#000000')).toEqual({ r: 0, g: 0, b: 0 });
-    }));
+    });
 
     it('should convert shorthand white hex to proper equivalent in rgb.', () => {
       expect(service.hexToRgb('#fff')).toEqual({ r: 255, g: 255, b: 255 });
-    }));
+    });
 
     it('should convert shorthand black hex to proper equivalent in rgb.', () => {
       expect(service.hexToRgb('#000')).toEqual({ r: 0, g: 0, b: 0 });
-    }));
+    });
   });
 
   describe('Functionality for the RGBToHex functionality', function() {
